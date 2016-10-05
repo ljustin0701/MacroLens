1ace96fea7b6438314a268d38af809ef3ae232ca
Christopher Yang <cyang001@citymail.cuny.edu>
2016-10-05 16:41:28 -0400

    Add get and post routes

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.get('/', function(req, res) {
     res.send("This is the index page");
 });
 
+app.post('/sign-up', function(req, res) {
+    // Handle sign-in
+    // If success show dashboard
+    // else return to index with an error message
+});
+
+app.post('/register', function(req, res) {
+    // Handle register
+    // if success show profile
+    // else return to index with an error message
+});
+
+app.get('/:id', function(req, res) {
+    res.send("This is the profile page for user id: " + req.params.id);
+});
+
 app.listen(port, function(){
     console.log("App listening on http://localhost:" + port);
 });
--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,5 +5,20 @@ router.get('/', function(req, res) {
     res.send("This is the dashboard page");
 });
 
+router.get('/new', function(req, res) {
+    res.send("This is the page for uploading a new photo");
+});
+
+router.post('/', function(req, res) {
+    // check if success upload pic then return to dashboard
+});
+
+router.get('/:photoID', function(req, res) {
+    res.send("This is the page for photo with id: " + req.params.photoID);
+});
+
+router.get('/:photoID/edit', function(req, res) {
+    res.send("This is the page for editing  photo with id: " + req.params.photoID);
+});
 
 module.exports = router;
