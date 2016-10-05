bd2da602002290f38acc7f4455a01fc938c1b269
Christopher Yang <cyang001@citymail.cuny.edu>
2016-10-05 16:47:23 -0400

    Add put and delete routes

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -21,4 +21,12 @@ router.get('/:photoID/edit', function(req, res) {
     res.send("This is the page for editing  photo with id: " + req.params.photoID);
 });
 
+router.put('/:photoID', function(req, res) {
+    // update a photo
+});
+
+router.delete('/:photoID', function(req, res) {
+    //delete a photo
+});
+
 module.exports = router;
