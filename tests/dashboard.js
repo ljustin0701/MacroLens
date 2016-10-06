ecf3522a303b0b397f936dbbe40284f63058cfe9
Justin Li <ljustin0701@gmail.com>
2016-10-06 11:06:40 -0400

    added basic tests

--- tests/dashboard.js
+++ tests/dashboard.js
@@ -6,11 +6,65 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Dashboard Controller', function() {
-	it('should show the dashboard on /dashboard GET'); // /dashboard
-	it('should show a page to upload a new photo GET'); // /dashboard/new
-	it('should be able to display a photo GET'); // /dashboard/:id
-	it('should be able to edit a photo GET'); // /dashboard/:id/edit
-	it('should be able to update a photo PUT'); // /dashboard/:id
-	it('should be able to delete a photo DELETE'); // /dashboard/:id
-	it('should be able to search for users GET'); ///dashboard/search/:username
+	it('should show the dashboard on /dashboard GET', (done) => {
+		chai.request(server)
+		.get('/dashboard')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			done();
+		});
+	}); // /dashboard
+	it('should show a page to upload a new photo GET', (done) => {
+		chai.request(server)
+		.get('/dashboard/new')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			done();
+		});
+	}); // /dashboard/new
+	it('should be able to display a photo GET', (done) => {
+		chai.request(server)
+		.get('/dashboard/:id')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':id')
+			done();
+		});
+	}); // /dashboard/:id
+	it('should be able to edit a photo GET', (done) => {
+		chai.request(server)
+		.get('/dashboard/:id/edit')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':id');
+			done();
+		});
+	}); // /dashboard/:id/edit
+	it('should be able to update a photo PUT', (done) => {
+		chai.request(server)
+		.get('/dashboard/:id')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':id');
+			done();
+		});
+	}); // /dashboard/:id
+	it('should be able to delete a photo DELETE', (done) => {
+		chai.request(server)
+		.get('/dashboard/:id')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':id');
+			done();
+		});
+	}); // /dashboard/:id
+	it('should be able to search for users GET', (done) => {
+		chai.request(server)
+		.get('/dashboard/search/:username')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':username');
+			done();
+		});
+	}); ///dashboard/search/:username
 });
--- tests/index.js
+++ tests/index.js
@@ -6,8 +6,37 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Index Controller', function() {
-	it('should load the homepage on / GET'); // /
-	it('should sign-in the user POST'); // /sign-in
-	it('should register the user POST'); // /register
-	it('should show the user profile page GET'); // /:id
+	it('should load the homepage on / GET', (done) => {
+		chai.request(server)
+		.get('/')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			done();
+		});
+	}); // /
+	it('should sign-in the user POST', (done) => {
+		chai.request(server)
+		.get('/sign-in')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			done();
+		});
+	}); // /sign-in
+	it('should register the user POST', (done) => {
+		chai.request(server)
+		.get('/register')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			done();
+		});
+	}); // /register
+	it('should show the user profile page GET', (done) => {
+		chai.request(server)
+		.get('/:id')
+		.end((err,res) => {
+			expect(res.status).to.equal(200);
+			expect(res.text).to.include(':id');
+			done();
+		});
+	}); // /:id
 });
