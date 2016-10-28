e9b18cde633e7ff1e24c7c94b582e0dd786c81d4
Justin Li <ljustin0701@gmail.com>
2016-10-28 02:45:14 -0400

    added handlebars but still cant find views

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+var models = require('./models');
+var bodyParser = require('body-parser');
 const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.engine('handlebars', exphbs({
+	layoutsDir: './views/layouts',
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', '${__dirname}/views/');
 
 var dashboard = require('./controllers/dashboard');
 app.use('/dashboard', dashboard);
@@ -16,10 +28,21 @@ app.post('/sign-up', function(req, res) {
     // else return to index with an error message
 });
 
-app.post('/register', function(req, res) {
+app.get('/register', function(req, res) {
+	res.render('register/register');
     // Handle register
     // if success show profile
     // else return to index with an error message
+});
+
+app.post('/register', function(req, res) {
+	console.log(req.body);
+	models.User.create({
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		username: req.body.username,
+		email: req.body.email
+	})
 });
 
 app.get('/:id', function(req, res) {
--- models/photo.js
+++ models/photo.js
@@ -2,14 +2,14 @@
 module.exports = function(sequelize, DataTypes) {
   var Photo = sequelize.define('Photo', {
     photo_id: DataTypes.INTEGER,
-    image: DataTypes.BYTEA,
+    image: DataTypes.BLOB,
     is_food: DataTypes.BOOLEAN,
     is_flagged: DataTypes.BOOLEAN,
     caption: DataTypes.TEXT,
-    calories: DataTypes.INT,
-    carbs: DataTypes.INT,
-    proteins: DataTypes.INT,
-    fats: DataTypes.INT
+    calories: DataTypes.INTEGER,
+    carbs: DataTypes.INTEGER,
+    proteins: DataTypes.INTEGER,
+    fats: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
