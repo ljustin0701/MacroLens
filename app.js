eaf6c3905778159f870d6beb3a2b494e2eb0b7fe
zachary_diemer <zdiemer21@gmail.com>
2016-11-18 09:33:59 -0500

    Commiting the work-in-progress passport app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-const models = require('./models');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+//const passport = require('./middlewares/authentication');
+//const viewHelpers = require('./middlewares/viewHelpers')
+const models = require('./models');
 
 var dashboard = require('./controllers/dashboard');
 var register = require('./controllers/register');
@@ -19,11 +23,18 @@ app.engine('handlebars', exphbs({
 	layoutsDir: './views/layouts',
 	defaultLayout: 'main'
 }));
-
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views/');
 
-// Controller uses
+//Set up static files
+app.use(express.static('./public'));
+
+//Setting up passport authentication
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(viewHelpers.register());
+
+//Controller uses - Should probably change these three lines to the way edgardo did it on the CTP blog 
 app.use('/dashboard', dashboard);
 app.use('/register', register);
 app.use('/signin', signin);
